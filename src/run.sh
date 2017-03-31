b187599644de83a8c8a2cfad88b49d4d2a6178c9
elenago <elena.ago@gmail.com>
2017-03-30 17:04:43 -0700

    Comm fix

--- src/run.sh
+++ src/run.sh
@@ -34,7 +34,6 @@ n run() {
         -x MP_DBREC_ON_GPU=0 \
         \
         -x GDS_DISABLE_WRITE64=1 -x GDS_SIMULATE_WRITE64=$A -x GDS_DISABLE_INLINECOPY=$B -x GDS_ENABLE_WEAK_CONSISTENCY=$C      -x GDS_DISABLE_MEMBAR=$D           \
-        -x CUDA_VISIBLE_DEVICES=0 -x CUDA_DISABLE_UNIFIED_MEMORY=0 \
         --mca btl_openib_want_cuda_gdr 1 --map-by node -np $NP -mca btl_openib_warn_default_gid_prefix 0 $PREFIX/src/scripts/wrapper.sh $PREFIX/src/lulesh-cuda-async/src/lulesh $PAR ) 2>&1 | tee -a run.log
 
 #-mca btl_openib_warn_default_gid_prefix 0
@@ -58,7 +57,7 @@ run 0 0 1 0 8 1 1 -s 60 -i 200  &> out_60_200.txt
 run 0 0 1 0 8 1 1 -s 60 -i 300  &> out_60_300.txt
 #COMMENT8
 
-#:<<COMMENT64
+:<<COMMENT64
 run 0 0 1 0 64 1 1 -s 60 -i 20 &> out_64p_60_20.txt
 run 0 0 1 0 64 1 1 -s 60 -i 70 &> out_64p_60_70.txt
 run 0 0 1 0 64 1 1 -s 60 -i 100 &> out_64p_60_100.txt
@@ -66,9 +65,9 @@ run 0 0 1 0 64 1 1 -s 60 -i 120 &> out_64p_60_120.txt
 run 0 0 1 0 64 1 1 -s 60 -i 170 &> out_64p_60_170.txt
 run 0 0 1 0 64 1 1 -s 60 -i 200 &> out_64p_60_200.txt
 run 0 0 1 0 64 1 1 -s 60 -i 300 &> out_64p_60_300.txt
-#COMMENT64
+COMMENT64
 
-#:<<COMMENT27
+:<<COMMENT27
 run 0 0 1 0 27 1 1 -s 60 -i 20 &> out_27p_60_20.txt
 run 0 0 1 0 27 1 1 -s 60 -i 70 &> out_27p_60_70.txt
 run 0 0 1 0 27 1 1 -s 60 -i 100 &> out_27p_60_100.txt
@@ -76,8 +75,4 @@ run 0 0 1 0 27 1 1 -s 60 -i 120 &> out_27p_60_120.txt
 run 0 0 1 0 27 1 1 -s 60 -i 170 &> out_27p_60_170.txt
 run 0 0 1 0 27 1 1 -s 60 -i 200 &> out_27p_60_200.txt
 run 0 0 1 0 27 1 1 -s 60 -i 300 &> out_27p_60_300.txt
-#COMMENT27
-
-~                                                                                                                                                                                                                                                                                         
-~                                                                                                                                                                                                                                                                                         
-~                         
+COMMENT27
